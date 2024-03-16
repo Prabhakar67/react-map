e91ded6e986e214b936275ceb648f1b07ae14dcb
prabhakar <prabhakaraher13@gmail.com>
2024-03-16 22:45:59 +0530

    Added code for react map revision

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+let nameArray = ['Ajay', 'Akshay', 'Ramesh', 'Suresh'];
+
+function elementToBeMapped(el, sn) {
+  return <>
+    <h2>{`${sn + 1}: ${el}`}</h2>
+  </>
+}
+
+// let elementToBeMapped = (el, sn) => (
+//   <h2>{`${sn + 1}: ${el}`}</h2>
+// )
+
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>{nameArray.map(elementToBeMapped)}</div>
     </div>
   );
 }
